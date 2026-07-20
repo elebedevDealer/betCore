d1a3eba2aee1295d7c63902e3f764bc46a5f3ff4
Egor Lebedev <egor.lebedev@betcore.eu>
2026-07-20 18:19:42 +0300

    7 homework fix

--- src/main/java/lesson_7/homework/Cat.java
+++ src/main/java/lesson_7/homework/Cat.java
@@ -1,8 +1,14 @@
 package lesson_7.homework;
 
-public class Cat {
+public class Cat extends Animal{
+        @Override
+        public void makeSound(){
+                System.out.println("mew, mew");
+        }
+
+
         public static void main(String[] args) {
-                Animal cat = new Animal();
+                Animal cat = new Cat();
                 cat.makeSound();
         }
 
--- src/main/java/lesson_7/homework/Dog.java
+++ src/main/java/lesson_7/homework/Dog.java
@@ -1,9 +1,14 @@
 package lesson_7.homework;
 
 
-public class Dog {
+public class Dog extends Animal{
+    @Override
+    public void makeSound (){
+        System.out.println("vaf, vaf");
+    }
+
     public static void main(String[] args) {
-        Animal dog = new Animal();
+        Animal dog = new Dog();
         dog.makeSound();
     }
 }
